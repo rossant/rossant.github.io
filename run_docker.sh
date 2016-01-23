aba39602c413907af3474d97dbbb4b95554bf9ba
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-01-23 20:41:32 +0100

    WIP: docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #sudo docker build -t pelican .
 docker pull rossant/pelican
-docker run -t --name=pelican-run -v $TRAVIS_BUILD_DIR:/site rossant/pelican
+docker run -t --name=pelican-run -v $(pwd):/site rossant/pelican
 docker rm pelican-run
